9fa534658ff054ca3bcfa62f5c6184f8f7618c43
vibhavk03 <vibhavkashyap3@gmail.com>
2023-10-02 17:00:25 +0530

    adding react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import Navbar from 'scenes/navbar';
+import ProfilePage from 'scenes/profilePage';
+
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={LoginPage} />
+          <Route path="/home" element={HomePage} />
+          <Route path="/profile/:userId" element={ProfilePage} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;

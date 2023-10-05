b234ef138832b0673090d785e3250ce55dc08cfd
vibhavk03 <vibhavkashyap3@gmail.com>
2023-10-05 21:59:48 +0530

    changing imports to components

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,9 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={LoginPage} />
-          <Route path="/home" element={HomePage} />
-          <Route path="/profile/:userId" element={ProfilePage} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
         </Routes>
       </BrowserRouter>
     </div>

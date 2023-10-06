3878864686c553a3244ff9daaeee5f34190760a2
vibhavk03 <vibhavkashyap3@gmail.com>
2023-10-06 22:15:22 +0530

    adding material ui theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+
 import HomePage from 'scenes/homePage';
 import LoginPage from 'scenes/loginPage';
 import Navbar from 'scenes/navbar';
 import ProfilePage from 'scenes/profilePage';
+import { themeSettings } from 'theme';
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  /* use memo will cache the value, it will only refresh when the mode value changes */
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline /> {/* css reset for material ui */}
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
